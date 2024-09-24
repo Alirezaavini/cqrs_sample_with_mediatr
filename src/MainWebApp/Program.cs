52e02b696caeeb3dd05bb19192270f1e84c69002
ikco\10201751 <a.moghaddami@ikco.ir>
2024-09-24 18:48:01 +0330

    add mediat r package

--- src/MainWebApp/Program.cs
+++ src/MainWebApp/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddMediatR(typeof(Program).Assembly);
 
 var app = builder.Build();
+
 
 app.MapGet("/", () => "Hello World!");
 
